098896f8aefc816428a266aedef64adff8579c71
Thijs Niks <thijs@niks.nu>
2019-11-14 21:05:33 -0800

    Added bash feedback

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-set -e
+set -eu
+
+echo "Starting FTP Deploy"
+
+echo "Using $INPUT_USER to connect to $INPUT_HOST"
+
+echo "Mirroring from $INPUT_LOCALDIR to $INPUT_REMOTEDIR"
 
 lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+
+echo "FTP Deploy Complete"
