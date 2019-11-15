0dcdfc242f3bae5870c85d6a1900cc61d4eed81a
Thijs Niks <thijs@niks.nu>
2019-11-14 20:52:08 -0800

    Continue when possible

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror -R -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror -R -c -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
