5b5c1425a21a17370d6cbcb947c2736b4f911cf9
Thijs Niks <thijs@niks.nu>
2019-11-15 17:48:52 -0800

    Revert "Added bash feedback"
    This reverts commit 098896f8aefc816428a266aedef64adff8579c71.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-set -eu
-
-echo "Starting FTP Deploy"
-
-echo "Using $INPUT_USER to connect to $INPUT_HOST"
-
-echo "Mirroring from $INPUT_LOCALDIR to $INPUT_REMOTEDIR"
+set -e
 
 lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
-
-echo "FTP Deploy Complete"
